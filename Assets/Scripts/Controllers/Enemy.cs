8efe6c8a5e10a45dcf9893b9ad31fda3809df504
Charcolate <ccfung0201@gmail.com>
2024-09-18 18:16:10 -0400

    going onto task 3

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,35 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform playerTransform; 
+    public float moveSpeed = 5f;      
+    public float stopDistance = 3f;  
+    public float randomOffsetRange = 20f; 
+
     private void Update()
     {
+        MoveEnemy();
     }
 
+    public void MoveEnemy()
+    {
+        Vector3 direction = playerTransform.position - transform.position;
+
+        Vector3 normalizedDirection = direction.normalized;
+
+        float distance = direction.magnitude;
+
+        Vector3 randomOffset = new Vector3(Random.Range(-randomOffsetRange, randomOffsetRange), Random.Range(-randomOffsetRange, randomOffsetRange),0f);
+
+        Vector3 movementWithOffset = normalizedDirection + randomOffset;
+
+        float movementFactor = 1;
+
+        if (distance <= stopDistance)
+        {
+            movementFactor = -1;
+        }
+       
+        transform.position += movementWithOffset.normalized * moveSpeed * Time.deltaTime * movementFactor;
+    }
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,10 +10,21 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
-    public float speed = 5f;
+    public float speed = 0f;
+
+    public float MaxSpeed = 20f;
+    public float AccelerationTime = 3f;
+    public float acceleration;
+
+    private float deceleration;
+    public float DecelerationTime = 5f;
 
     void Update()
     {
+        acceleration = MaxSpeed / AccelerationTime;
+
+        deceleration = MaxSpeed / DecelerationTime;
+        
         PlayerMovement();
 
     }
@@ -23,7 +34,19 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical, 0f);
+        Vector3 movement = new Vector3(horizontal, vertical, 0f).normalized;
+
+        if (movement.magnitude > 0)
+        {
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Min(speed, MaxSpeed);
+        }
+
+        if (movement.magnitude == 0 && speed > 0)
+        {
+            speed -= deceleration * Time.deltaTime;
+            speed = Mathf.Max(speed, 0);
+        }
 
         transform.Translate(movement * speed * Time.deltaTime);
     }
