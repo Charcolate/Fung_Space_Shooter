0e4d73a6fcd0503630a84cdfd6c04a1e5112e6b6
Charcolate <ccfung0201@gmail.com>
2024-09-18 18:49:26 -0400

    Update Asteroid.cs
    finished task 3, will continue to work on task 4 tomorrow

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,36 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    private Vector3 targetPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        ChooseRandomPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MoveTowardsTarget();
+
+
+        if (Vector3.Distance(transform.position, targetPoint) <= arrivalDistance)
+        {
+            ChooseRandomPoint();
+        }
+    }
+
+
+    void ChooseRandomPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * maxFloatDistance;
+        randomDirection.z = 0;
+        targetPoint = transform.position + randomDirection;
+    }
+
+    void MoveTowardsTarget()
+    {
+        Vector3 direction = (targetPoint - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
     }
 }
